ab5c2f61d689e4976918465698a654dcb9bace9f
mauricio-alves <mauricioalvesnutri@gmail.com>
2022-07-25 17:07:13 -0300

    template pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Signup } from "./pages/Signup";
-import { AuthContextComponent } from "./contexts/authContext";
-import { Profile } from "./pages/Profile";
 import { ErrorPage } from "./pages/ErrorPage";
-
+import { AuthContextComponent } from "./contexts/authContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
-
 import "./global.css";
+import { PatientUser } from "./pages/User/user";
+import { NutriUser } from "./pages/User/admin";
+import { NutriLogin } from "./pages/Login/admin";
+import { UserLogin } from "./pages/Login/user";
+import { UserSignup } from "./pages/Signup/user";
+import { AdminSignup } from "./pages/Signup/admin";
+import { UserProfile } from "./pages/Profile/user";
+import { AdminProfile } from "./pages/Profile/admin";
 
 export function App() {
   return (
@@ -16,11 +19,21 @@ export function App() {
       <AuthContextComponent>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+
+          <Route path="/user" element={<PatientUser />} />
+          <Route path="/user/login" element={<UserLogin />} />
+          <Route path="/user/signup" element={<UserSignup />} />
           <Route
-            path="/profile"
-            element={<ProtectedRoute component={Profile} />}
+            path="/user/profile"
+            element={<ProtectedRoute component={<UserProfile />} />}
+          />
+
+          <Route path="/admin" element={<NutriUser />} />
+          <Route path="/admin/login" element={<NutriLogin />} />
+          <Route path="/admin/signup" element={<AdminSignup />} />
+          <Route
+            path="/admin/profile"
+            element={<ProtectedRoute component={<AdminProfile />} />}
           />
 
           <Route path="*" element={<ErrorPage />} />
--- src/pages/User/user.js
+++ src/pages/User/user.js
@@ -1,39 +1,30 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/ei_nutri_logo.jpg";
 
-import React, { useState } from "react";
-import moment from "moment";
-import Calendar from "../../components/Calendar/index.js";
-
-export function Home() {
-  const [value, setValue] = useState(moment());
-
+export function PatientUser() {
   return (
     <div className="bg-amber-600 text-white h-screen">
       <div className="flex justify-center pt-52">
         <img src={logo} alt="ei nutri logo" className="h-24 rounded-full" />
       </div>
       <div>
-        <h3 className="text-2xl mt-12 text-center">Qual o seu perfil?</h3>
+        <h3 className="text-2xl mt-12 text-center">Tela do paciente</h3>
       </div>
       <div className="flex-col text-center text-black mt-8 justify-center">
         <div>
-          <Link to="/user">
+          <Link to="/user/signup">
             <button className="items-center justify-center px-9 py-2 border border-transparent text-base font-medium mb-2 rounded-full  bg-indigo-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-100 duration-300">
-              Paciente
+              Cadastrar
             </button>
           </Link>
         </div>
         <div>
-          <Link to="/admin">
+          <Link to="/user/login">
             <button className="items-center justify-center px-5 py-2 border border-transparent text-base font-medium rounded-full  bg-indigo-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-100 duration-300">
-              Nutricionista
+              Entrar
             </button>
           </Link>
         </div>
-      </div>
-      <div>
-        <Calendar value={value} onChange={setValue} />
       </div>
     </div>
   );
