b927214ad1b27ce6686f65c22589b1691acd8a0d
mauricio-alves <mauricioalvesnutri@gmail.com>
2022-07-29 01:44:14 -0300

    creating and deleting reviews

--- src/pages/Profile/user.js
+++ src/pages/Profile/user.js
@@ -3,6 +3,7 @@ import { api } from "../../api/api";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/images/ei_nutri_logo.jpg";
 import toast, { Toaster } from "react-hot-toast";
+import { ReviewCard } from "../../components/ReviewCard";
 
 export function PatientProfile() {
   const [user, setUser] = useState({ name: "", email: "" });
@@ -20,7 +21,7 @@ export function PatientProfile() {
       }
     }
     fetchUser();
-  }, []);
+  }, [handlerefresh]);
 
   function handleLogOut() {
     localStorage.removeItem("loggedInUser");
@@ -35,11 +36,17 @@ export function PatientProfile() {
     navigate("/user/catalog");
   }
 
+  function handlerefresh() {
+    navigate("/user/profile");
+  }
+
   async function handleDelete() {
     try {
       await api.delete("/user/disable-profile");
       toast.success("Conta deletada com sucesso!");
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
     navigate("/");
   }
 
@@ -101,10 +108,17 @@ export function PatientProfile() {
         <div>
           <div className="block text-lg font-bold mt-6 mb-2 pl-4">
             <h2>Suas consultas:</h2>
-            {}
+            <p>{user.user.appointments[0].slice(0, 21)} hrs.</p>
           </div>
           <div className="block text-lg font-bold mt-6 mb-2 pl-4">
             <h2>Seus reviews:</h2>
+            {user.user.reviews.map((currentReview) => {
+              return (
+                <div onClick={handlerefresh} key={currentReview._id}>
+                  <ReviewCard props={currentReview} />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
