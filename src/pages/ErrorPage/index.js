38c7621347b4abff8d7978a63e704642effdd0a3
mauricio-alves <mauricioalvesnutri@gmail.com>
2022-07-29 02:03:16 -0300

    cleaning some consoles and errorpage

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,8 +1,17 @@
+import errorImg from "../../assets/images/errorpage.jpg";
+import logo from "../../assets/images/ei_nutri_logo.jpg";
+
 export function ErrorPage() {
   return (
-    <img
-      src="https://thumbor.forbes.com/thumbor/711x476/https://blogs-images.forbes.com/zarastone/files/2017/05/Waffles-The-Dog-Amazon-1200x804.jpg?width=960"
-      alt="Error"
-    />
+    <>
+      <div className="flex justify-center pt-12 pb-6">
+        <img
+          src={logo}
+          alt="ei nutri logo"
+          className="sm:h-24 md:h-40 lg:h-56 rounded-full"
+        />
+      </div>
+      <img className="h-full w-full" src={errorImg} alt="Error" />;
+    </>
   );
 }
